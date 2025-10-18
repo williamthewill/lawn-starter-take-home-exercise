f82e5c830cdeab7690b7446727f4e238015dc6f1
William Muller <williammuller-code@hotmail.com>
2025-10-18 15:30:53 -0300

    wip: cleaning project

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Middleware\HandleAppearance;
+// use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\LogGraphQLToDatabase;
 use Illuminate\Foundation\Application;
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
         
         $middleware->web(append: [
-            HandleAppearance::class,
+            // HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,4 @@ Route::get('/', function () {
 // });
 
 require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php'; 
